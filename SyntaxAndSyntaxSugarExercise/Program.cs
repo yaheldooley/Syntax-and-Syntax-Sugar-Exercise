56de9d1fb2a485f94a297312aa235d4e97009b21
yahel dooley <ydooley@gmail.com>
2022-10-18 14:48:33 -0700

    Implemented inferred typing, string interpolation and made if else inline (ternary)

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			var answer = 4;
+			string response;
+
+			response = answer < 9 ? $"{answer} is less than nine" : 
+									$"{answer} is greater than or equal to nine";
+
+			Console.WriteLine(response);
+		}
     }
 }
